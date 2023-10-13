316d113ad25e0ae99d1d75c554dbb3d2a07c6c2a
Ian Schroeder <schroeder.ian@student.greenriver.edu>
2023-10-13 18:46:27 -0400

    added views folder and home.php

--- index.php
+++ index.php
@@ -10,5 +10,11 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+//Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Hello, world!</h1>';
+}
+);
+
 //Run fat free
 $f3->run();
