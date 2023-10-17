7b4f98ab478e2d669786bbb9071092f623543b64
Ian Schroeder <schroeder.ian@student.greenriver.edu>
2023-10-16 22:17:30 -0400

    added styles and scripts

--- tmp/zaf82r0bzlct.3m34iyblx4mcs.php
+++ tmp/zaf82r0bzlct.3m34iyblx4mcs.php
@@ -3,9 +3,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Board</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<h1>Natural Resources Job Board</h1>
+<h1>Natural Resource Job Board</h1>
+<table id="job-table">
+    <thead>
+    <tr>
+        <th>Title</th>
+        <th>Company</th>
+        <th>Category</th>
+        <th>Location</th>
+        <th>Date Posted</th>
+        <th>Expires</th>
+        <th>Apply Now</th>
+    </tr>
+    </thead>
+    <tbody>
+    <!-- Job listings will be inserted here dynamically -->
+    </tbody>
+</table>
+<script src="script.js"></script>
 </body>
 </html>
