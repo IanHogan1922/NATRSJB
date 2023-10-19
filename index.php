1432bcb17736615fee8a23d8337a5395a3a8426e
SlavikKhrapach <slavikkhrapach@gmail.com>
2023-10-19 17:41:13 -0400

    Merged design and functionality versions

--- index.php
+++ index.php
@@ -12,11 +12,20 @@ $f3 = Base::instance();
 
 //Define a default route
 $f3->route('GET /', function() {
-    $view = new Template();
-    echo $view->render
-    ('views/home.php');
-}
-);
+    $view = new View(); // View
+    echo $view->render('views/home.php');
+
+});
+
+$f3->route('GET|POST /add_jobs', function() { // don't forget "GET|POST"
+    $view = new View();
+    echo $view->render('views/dataEntry.php');
+
+});
+
+
+
+
 
 //Run fat free
 $f3->run();
