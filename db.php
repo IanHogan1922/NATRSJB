e1e789e23dcd75c9c0d556300ed630f4b2963da9
SlavikKhrapach <slavikkhrapach@gmail.com>
2023-10-23 15:55:36 -0400

    pushing to clear git status

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$username = 'natrsjob';
-$password = 'MMU6[0bjxm8O7(';
-$hostname = 'localhost';
-$database = 'natrsjob_db';
-$cnxn = @mysqli_connect($hostname, $username, $password, $database) or
-die("Error Connecting to DB: " . mysqli_connect_error());
-echo 'connected to Database!';
