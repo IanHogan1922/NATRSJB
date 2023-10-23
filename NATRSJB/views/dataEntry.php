439b4be7599bb0c9d087391a96362845c4f26606
SlavikKhrapach <slavikkhrapach@gmail.com>
2023-10-23 16:55:55 -0400

    testing changes to dataEntry

--- NATRSJB/views/dataEntry.php
+++ NATRSJB/views/dataEntry.php
@@ -24,39 +24,7 @@
             <p>The Job Title: <input placeholder="Job Title" type="text" name="title"></p>
             <p>Employment Status: <input placeholder="Full-time, Part-time, Seasonal" type="text" name="status"></p>
             <p>Company Name: <input placeholder="Company Name" type="text" name="company"></p>
-            <div class="col-sm-12">
-                <div class="col-xs-12 input-group">
-                    <fieldset class="form-group" id="checkbox-display">
-                        <h4>Applicable Career Track:<h4>
-                                <div class="col-xs-12">
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="category[]" value="GIS"><span>GIS</span></label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="category[]" value="Forestry"><span>Forestry</span></label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="category[]" value="Water Quality"><span>Water Quality</span></label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="category[]" value="Park Management"><span>Park Management</span></label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="category[]" value="Restoration"><span>Restoration</span></label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="category[]" value="Conservation"><span>Conservation</span></label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="category[]" value="Fish and Wildlife"><span>Fish and Wildlife</span></label>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="category[]" value="Wildland Fire"><span>Wildland Fire</span></label>
-                                    </div>
-                                </div>
-                    </fieldset>
-                </div>
-            </div>
+
             <p>Location: <input placeholder="Location" type="text" name="location"></p>
             <p>Post Expiration: <input type="date" name="expiration"></p>
             <p>Permanent Position: <input placeholder="1 or 0" type="text" name="permanent"></p>
