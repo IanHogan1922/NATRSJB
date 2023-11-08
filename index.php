16f284a7a2df0c816dd816318ddb048367bb8193
SlavikKhrapach <slavikkhrapach@gmail.com>
2023-11-08 18:58:57 -0500

    Added announcements page

--- index.php
+++ index.php
@@ -17,6 +17,21 @@ $f3->route('GET /', function() {
 });
 
 $f3->route('GET|POST /add_jobs', function() { // don't forget "GET|POST"
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $title = isset($_POST['title']) ? $_POST['title'] : "";
+        $status = isset($_POST['status']) ? $_POST['status'] : "";
+        $company = isset($_POST['company']) ? $_POST['company'] : "";
+        $category = isset($_POST['category']) ? $_POST['category'] : "";
+        $data = isset($_POST['category']) ? $_POST['category'] : "";
+        $location = isset($_POST['location']) ? $_POST['location'] : "";
+        $expiration = isset($_POST['expiration']) ? $_POST['expiration'] : "";
+        $permanent = isset($_POST['permanent']) ? $_POST['permanent'] : "";
+        $internship = isset($_POST['internship']) ? $_POST['internship'] : "";
+        $paid = isset($_POST['paid']) ? $_POST['paid'] : "";
+
+    }
     $view = new View();
     echo $view->render('views/dataEntry.php');
 });
@@ -24,6 +39,11 @@ $f3->route('GET|POST /add_jobs', function() { // don't forget "GET|POST"
 $f3->route('GET /login', function() {
     $view = new View(); // View
     echo $view->render('views/login.php');
+});
+
+$f3->route('GET /announcements', function() {
+    $view = new View(); // View
+    echo $view->render('views/announcements.php');
 });
 
 
--- views/announcements.php
+++ views/announcements.php
@@ -8,7 +8,40 @@ ini_set('display_errors', 1);
 include 'views/modules/header.html';
 ?>
 
+<div class="container">
+    <div id ="announcements" class="col-xs-12">
+        <div class="col-xs-12 split-top">
+            <h2 class="text-center">Announcements</h2>
+            <?php
 
+            $sql = "SELECT * FROM announcements WHERE visibility != '0'";
+
+            $result = @mysqli_query($cnxn, $sql);
+
+            while($row = @mysqli_fetch_assoc($result)) {
+                $date = $row['date'];
+                $description = $row['description'];
+                $title = $row['title'];
+                $visibility = $row['visibility'];
+
+
+
+                echo "
+                <div class='panel panel-success'>
+                <div class='panel-heading'>
+                    <strong>$title</strong>
+                    <span class='pull-right'><strong>$date</strong></span>
+                    <div class='panel-body wrap-text'>
+                        $description
+                    </div>
+                </div>
+            </div>
+            ";
+            }
+            ?>
+        </div>
+    </div>
+</div>
 
 
 
