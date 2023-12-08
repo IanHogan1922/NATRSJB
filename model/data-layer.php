04e628a43efb37487e4beccc55d19ed940793d2a
Ian Schroeder <schroeder.ian@student.greenriver.edu>
2023-12-08 03:55:05 -0500

    added delete function to data-layer and delet column to homepage

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,33 +1,6 @@
 <?php
     session_start();
 
-/*  NATRSLB/model/data-layer.php
-
-    DROP TABLE orders;
-    CREATE TABLE orders (
-        order_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        food VARCHAR(50) NOT NULL,
-        meal VARCHAR(20) NOT NULL,
-        condiments VARCHAR(100),
-        date_time DATETIME DEFAULT NOW()
-    );
-    INSERT INTO orders (food, meal, condiments)
-    VALUES ('burrito', 'breakfast', 'salsa'),
-           ('pad thai', 'dinner', NULL),
-           ('tacos', 'lunch', 'pork, salsa, guac, sour cream')
-
-     * PDO - Using Prepared Statements
-        1. Define the query (test first!)
-            $sql = “…”;
-        2. Prepare the statement
-            $statement = $dbh->prepare($sql);
-        3. Bind the parameters
-            $statement->bindParam(param_name, value, type);
-        4. Execute
-            $statement->execute();
-        5. Process the result, if there is one
-*/
-
 //echo $_SERVER['DOCUMENT_ROOT'];
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../pdo-config.php');
 //include('views/server.php');
@@ -169,6 +142,15 @@ class DataLayer
         if ($result) {
             echo "Fail";
         }
+    }
+
+    function hideJob($jobNumber) {
+        $sql = "UPDATE jobboard2 SET visibility = 0 WHERE job_number = :job_number";
+        $statement = $this->_dbh->prepare($sql);
+        $statement->bindParam(':job_number', $jobNumber, PDO::PARAM_INT);
+        $result = $statement->execute();
+
+        return $result;
     }
 
     function signIn($email, $password) {
