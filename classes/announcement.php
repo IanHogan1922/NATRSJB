4b80a29c448e42255fdb5262ea7e282aa03b26d3
SlavikKhrapach <slavikkhrapach@gmail.com>
2023-12-11 14:49:19 -0500

    Added comments and finished PHPDoc comments

--- classes/announcement.php
+++ classes/announcement.php
@@ -2,35 +2,39 @@
 
 namespace classes;
 
+//Start of Class
 class announcement
 {
-
+    //fields
     private $title;
     private $description;
     private $visibility;
 
     /**
-     * @param $title
-     * @param $description
-     * @param $visibility
+     * Constructs an announcement object instance based on the job title, description, and visibility.
+     * @param string $title The title of the job announcement
+     * @param string $description A description of the job announcement.
+     * @param bool $visibility The visibility status of the Job Announcement.  (e.g. admin-level or user-level).
      */
+    //Constructor
     public function __construct($title, $description, $visibility)
     {
         $this->title = $title;
         $this->description = $description;
         $this->visibility = $visibility;
     }
-
+    //Getters and Setters
     /**
-     * @return mixed
+     * Gets the title of the job posting announcement.
+     * @return string The title of the job posting announcement.
      */
     public function getTitle()
     {
         return $this->title;
     }
-
     /**
-     * @param mixed $title
+     * Sets the title of the job posting announcement.
+     * @param string $title Sets the title of the job posting announcement.
      */
     public function setTitle($title)
     {
@@ -38,7 +42,8 @@ class announcement
     }
 
     /**
-     * @return mixed
+     * Gets the description of the job posting announcement.
+     * @return string The description of the job posting announcement.
      */
     public function getDescription()
     {
@@ -46,7 +51,8 @@ class announcement
     }
 
     /**
-     * @param mixed $description
+     * Sets the description of the job posting announcement.
+     * @param string $description Sets the description of the job posting announcement.
      */
     public function setDescription($description)
     {
@@ -54,7 +60,8 @@ class announcement
     }
 
     /**
-     * @return mixed
+     * Gets the visibility status of the Job Announcement.  (e.g. admin-level or user-level).
+     * @return bool The visibility status of the Job Announcement (e.g. admin-level or user-level
      */
     public function getVisibility()
     {
@@ -62,13 +69,12 @@ class announcement
     }
 
     /**
-     * @param mixed $visibility
+     * Sets the visibility status of the Job Announcement.
+     * @param bool $visibility Sets the visibility status of the Job Announcement.
      */
     public function setVisibility($visibility)
     {
         $this->visibility = $visibility;
     }
-
-
-
 }
+//End of class
