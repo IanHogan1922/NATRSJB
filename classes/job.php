b9c8842cdc56acca3d32008d4dfad390fb713c7e
SlavikKhrapach <slavikkhrapach@gmail.com>
2023-12-11 16:05:12 -0500

    Added PHPDoc and regular comments to job.php

--- classes/job.php
+++ classes/job.php
@@ -2,8 +2,10 @@
 
 namespace classes;
 
+//Start of class
 class job {
 
+    //Fields
     private $title;
     private $status;
     private $company;
@@ -19,16 +21,19 @@ class job {
 
 
     /**
-     * @param $title
-     * @param $status
-     * @param $company
-     * @param $category
-     * @param $location
-     * @param $expiration
-     * @param $permanent
-     * @param $internship
-     * @param $paid
+     * Constructs a job object instance based on the job title, status,company, category, location, expiration,
+     * permanent, internship and paid.
+     * @param string $title The title of the job.
+     * @param bool $status The current status of the job.
+     * @param string $company The company that the job is being offered from.
+     * @param string $category The category of the job.
+     * @param string $location The job's location.
+     * @param string $expiration  The expiration date of the job.
+     * @param bool $permanent If the job is a permanent position.
+     * @param bool $internship  If the job is an internship.
+     * @param bool $paid If the job is paid or not.
      */
+    //Constructor
     public function __construct($title, $status, $company, $category, $location, $expiration, $permanent, $internship, $paid, $url, $visibility)
     {
         $this->title = $title;
@@ -43,9 +48,10 @@ class job {
         $this->url = $url;
         $this->visibility = $visibility;
     }
-
+    //Getters and Setters
     /**
-     * @return mixed
+     * Gets the title of the job.
+     * @return string The title of the job.
      */
     public function getTitle()
     {
@@ -53,7 +59,8 @@ class job {
     }
 
     /**
-     * @param mixed $title
+     * Sets the title of the job.
+     * @param string $title The title of the job.
      */
     public function setTitle($title)
     {
@@ -61,7 +68,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the status of the job, true if the job is active, false otherwise.
+     * @return bool The status of the job.
      */
     public function getStatus()
     {
@@ -69,7 +77,8 @@ class job {
     }
 
     /**
-     * @param mixed $status
+     * Sets the status of the job, true if the job is active, false otherwise.
+     * @param bool $status Sets the status of the job.
      */
     public function setStatus($status)
     {
@@ -77,7 +86,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the company that is offering the job.
+     * @return string The company that is offering the job.
      */
     public function getCompany()
     {
@@ -85,7 +95,8 @@ class job {
     }
 
     /**
-     * @param mixed $company
+     * Sets the company that is offering the job.
+     * @param string $company The company that is offering the job.
      */
     public function setCompany($company)
     {
@@ -93,7 +104,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the category of the job.
+     * @return string The category of the job.
      */
     public function getCategory()
     {
@@ -101,7 +113,8 @@ class job {
     }
 
     /**
-     * @param mixed $category
+     * Sets the category of the job.
+     * @param string $category The category of the job.
      */
     public function setCategory($category)
     {
@@ -109,7 +122,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the location of the job.
+     * @return string The location of the job.
      */
     public function getLocation()
     {
@@ -117,7 +131,8 @@ class job {
     }
 
     /**
-     * @param mixed $location
+     * Sets the location of the job.
+     * @param string $location The location of the job.
      */
     public function setLocation($location)
     {
@@ -125,7 +140,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the expiration date of the job.
+     * @return string The expiration date of the job.
      */
     public function getExpiration()
     {
@@ -133,7 +149,8 @@ class job {
     }
 
     /**
-     * @param mixed $expiration
+     * Sets the expiration date of the job.
+     * @param string $expiration The expiration date of the job.
      */
     public function setExpiration($expiration)
     {
@@ -141,7 +158,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the permanent status of the job (e.g. Is it a permanent job or not).
+     * @return bool The type status of the job (e.g. Is it a permanent job or not).
      */
     public function getPermanent()
     {
@@ -149,7 +167,8 @@ class job {
     }
 
     /**
-     * @param mixed $permanent
+     * Sets the permanent status of the job (e.g. True if permanent job, false otherwise).
+     * @param bool $permanent The type status of the job.
      */
     public function setPermanent($permanent)
     {
@@ -157,7 +176,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the internship status of the job (e.g. Is it an internship job or not).
+     * @return bool The internship status of the job.
      */
     public function getInternship()
     {
@@ -165,7 +185,8 @@ class job {
     }
 
     /**
-     * @param mixed $internship
+     * Sets the internship status, true if the job is an internship, false otherwise.
+     * @param bool $internship The internship status of the job.
      */
     public function setInternship($internship)
     {
@@ -173,7 +194,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the pay status of the job, true if paid position, false otherwise.
+     * @return bool The pay status of the job.
      */
     public function getPaid()
     {
@@ -181,7 +203,8 @@ class job {
     }
 
     /**
-     * @param mixed $paid
+     * Sets the pay status of the job, true if paid position, false otherwise.
+     * @param bool $paid The pay status of the job.
      */
     public function setPaid($paid)
     {
@@ -189,7 +212,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the URL location of the original job posting.
+     * @return string The URL location of the original job posting.
      */
     public function getUrl()
     {
@@ -197,7 +221,8 @@ class job {
     }
 
     /**
-     * @param mixed $url
+     * Sets the URL location of the original job posting.
+     * @param string $url The URL location of the original job posting.
      */
     public function setUrl($url)
     {
@@ -205,7 +230,8 @@ class job {
     }
 
     /**
-     * @return mixed
+     * Gets the visibility status of the job.
+     * @return bool The visibility status of the job.
      */
     public function getVisibility()
     {
@@ -213,12 +239,12 @@ class job {
     }
 
     /**
-     * @param mixed $visibility
+     * Sets the visibility status of the job, true if visible, false otherwise.
+     * @param bool $visibility The visibilty status of the job.
      */
     public function setVisibility($visibility)
     {
         $this->visibility = $visibility;
     }
-
-
 }
+//End of class
