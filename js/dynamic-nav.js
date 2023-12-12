4cb167f97584d3198006197ec379b7c091396418
SlavikKhrapach <slavikkhrapach@gmail.com>
2023-12-11 23:03:49 -0500

    Made navbar to highlite current page

--- js/dynamic-nav.js
+++ js/dynamic-nav.js
@@ -1,14 +1,24 @@
-$(document).ready(function()
-{
-   if ($(location).attr('pathname') == '/') {
-        $('#home-link').addClass('current');
-   }
-   
-   if ($(location).attr('pathname') == '/post-job') {
-        $('#post-link').addClass('current');
-   }
-   
-   if ($(location).attr('pathname') == '/announcements') {
-        $('#annoucement-link').addClass('current');
-   }   
+document.addEventListener('DOMContentLoaded', function() {
+    const path = window.location.pathname;
+
+    if (path === '/NATRSJB/') {
+        const homeLink = document.getElementById('home-link');
+        homeLink.classList.add('current');
+    }
+    if (path === '/NATRSJB/post-job') {
+        const postLink = document.getElementById('post-link');
+        postLink.classList.add('current');
+    }
+    if (path === '/NATRSJB/announcements') {
+        const announcementLink = document.getElementById('announcements-link');
+        announcementLink.classList.add('current');
+    }
+    if (path === '/NATRSJB/add_jobs') {
+        const announcementLink = document.getElementById('newJob-link');
+        announcementLink.classList.add('current');
+    }
+    if (path === '/NATRSJB/newAnnouncement') {
+        const announcementLink = document.getElementById('newAnnouncement-link');
+        announcementLink.classList.add('current');
+    }
 });
